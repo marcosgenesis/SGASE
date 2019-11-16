bbd680e64946d9781e5f8f0c7b3a9662408434bf
Gustavo Colombo <gusttavocolombo@gmail.com>
2019-11-16 12:59:56 -0300

    feature-cadastro-coordenador

--- SGASE/src/br/com/ufc/es/poo/model/Administrador.java
+++ SGASE/src/br/com/ufc/es/poo/model/Administrador.java
@@ -1,14 +1,32 @@
 
 package br.com.ufc.es.poo.model;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Administrador extends Usuario {
 
+    private ArrayList<Coordenador> coordenador = new ArrayList <Coordenador>();
+    
     public Administrador(String cpf, int id, String email, String nome) {
         super(cpf, id, email, nome);
+        this.coordenador = coordenador;
     }
 
-    public void cadastraCoordenador() {
-        return ;
+    public ArrayList<Coordenador> getCoordenador() {
+        return coordenador;
+    }
+    
+    public void setCoordenador(ArrayList<Coordenador> coordenador) {
+        this.coordenador = coordenador;
+    }
+     public void cadastraCoordenador(Coordenador p) {
+        int i = 0;
+        for(i=0; i <coordenador.size(); i++ ){
+            if(coordenador.get(i).getCpf().equals(p.getCpf())){
+                System.out.println("Já existe um coordenador com esse cpf");
+            }else{
+                coordenador.add(p);
+            }
+        }
     }    
 }
