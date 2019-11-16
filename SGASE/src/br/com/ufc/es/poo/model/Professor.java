d183eb99f52ce60d12cae20ac9276e8a1358157a
Gustavo Colombo <gusttavocolombo@gmail.com>
2019-11-16 15:00:21 -0300

    feature-cadastrar-presenca-do-aluno feature-cadastrar-falta-do-aluno

--- SGASE/src/br/com/ufc/es/poo/model/Professor.java
+++ SGASE/src/br/com/ufc/es/poo/model/Professor.java
@@ -9,18 +9,26 @@ public class Professor extends Usuario {
         super(cpf, id, email, nome);
     }
 
-    public void falta(AlunoBolsista p){
-        int i = 0;
+    public int falta(AlunoBolsista p){
+        int i = 0, j =0; int totalFaltas = 0;
         for(i =0; i < p.faltas.length ;i++){
-            p.presenca[i] += 2;
+            p.faltas[i] += 2;
         }
+        for(j = 0; j < p.faltas.length; j ++){
+            totalFaltas += p.faltas[i];
+        }
+        return totalFaltas;
     }
     
-    public void presenca(AlunoBolsista p){
-        int i = 0;
+    public int presenca(AlunoBolsista p){
+        int i = 0, j = 0; int totalPresenca = 0;
          for(i=0; i < p.presenca.length; i++){
              p.presenca[i] += 2;
         }
+         for(j = 0; j < p.presenca.length; j ++){
+            totalPresenca += p.presenca[i];
+        }
+        return totalPresenca;
     }
  
    
