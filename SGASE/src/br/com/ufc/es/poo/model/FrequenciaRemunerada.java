1a79996d8186d64d3184c3712025defd3bd0cb4e
Gustavo Colombo <gusttavocolombo@gmail.com>
2019-11-16 22:07:21 -0300

    fix-classe-frequencia-remunerada

--- SGASE/src/br/com/ufc/es/poo/model/FrequenciaRemunerada.java
+++ SGASE/src/br/com/ufc/es/poo/model/FrequenciaRemunerada.java
@@ -6,4 +6,47 @@ public class FrequenciaRemunerada {
     private String mesRefefente;
     private String nomeBolsista;
     private String curso;
+    private Professor professor;
+
+    public FrequenciaRemunerada(String mesRefefente, String nomeBolsista, String curso, Professor professor) {
+       this.mesRefefente = mesRefefente;
+       this.nomeBolsista = nomeBolsista;
+       this.curso = curso;
+       this.professor = professor;
+    }
+      
+    public String getMesRefefente() {
+        return mesRefefente;
+    }
+
+    public void setMesRefefente(String mesRefefente) {
+        this.mesRefefente = mesRefefente;
+    }
+
+    public String getNomeBolsista() {
+        return nomeBolsista;
+    }
+
+    public void setNomeBolsista(String nomeBolsista) {
+        this.nomeBolsista = nomeBolsista;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+    
+    
+    
 }
