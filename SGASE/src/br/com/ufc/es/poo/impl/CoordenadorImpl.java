35ab58e2b3a16ca891ecf23e6abbb2ca2e4fc616
Gustavo Colombo <gusttavocolombo@gmail.com>
2019-11-16 23:45:09 -0300

    feature-interface-CoordenadorImpl

---
+++ SGASE/src/br/com/ufc/es/poo/impl/CoordenadorImpl.java
@@ -0,0 +1,47 @@
+
+package br.com.ufc.es.poo.impl;
+//import br.com.ufc.es.poo.model.*;
+import br.com.ufc.es.poo.Interfaces.*;
+import br.com.ufc.es.poo.model.AlunoBolsista;
+import br.com.ufc.es.poo.model.Professor;
+import br.com.ufc.es.poo.model.Turma;
+import br.com.ufc.es.poo.model.Usuario;
+
+
+public class CoordenadorImpl extends Usuario implements Coordenador{
+
+    
+    public CoordenadorImpl(String cpf, int id, String email, String nome) {
+        super(cpf, id, email, nome);
+    }
+
+    public void verificaFrequenciaRemunerada() {
+        //
+    }
+
+    public void cadastraProfessor(Turma turma, Professor professor) {
+        turma.getCurso().addProfessor(professor);
+    }
+    
+    public void editaProfessor(Turma turma, Professor professorNovo,Professor professorAntigo) {
+        turma.getCurso().setProfessor(professorNovo,professorAntigo);
+    }
+  
+    public void removeProfessor(Turma turma,Professor professorRemovido) {
+        turma.getCurso().getProfessores().remove(professorRemovido);
+    }
+
+    public void cadastraAluno(Turma turma,AlunoBolsista alunoCadastrado) {
+        turma.getCurso().getParticipantes().add(alunoCadastrado);
+    }
+
+    public void editaAluno(Turma turma) {
+    
+    }
+
+    public void removeAluno() {
+      //
+    }
+    
+}
+    
