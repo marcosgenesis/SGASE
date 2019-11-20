d49088c9c1d700db55e44990084e1dd91ae5df48
Gustavo Colombo <gusttavocolombo@gmail.com>
2019-11-20 14:33:08 -0300

    feature-classe-Excpetion

---
+++ SGASE/src/br/com/ufc/es/poo/exception/ValidaCpfException.java
@@ -0,0 +1,18 @@
+
+package br.com.ufc.es.poo.exception;
+
+
+public class ValidaCpfException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ValidaCpfException() {
+		super();
+	}
+	
+	public ValidaCpfException(String message) {
+		super(message);
+	}
+}
+
+
+
