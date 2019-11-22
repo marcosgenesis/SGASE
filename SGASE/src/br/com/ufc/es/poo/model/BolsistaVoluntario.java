fd884ea85bcb015c507783d2a19020c0265688a2
Gustavo Colombo <gusttavocolombo@gmail.com>
2019-11-22 20:09:47 -0300

    fix-construtor-bolsista-voluntario

--- SGASE/src/br/com/ufc/es/poo/model/BolsistaVoluntario.java
+++ SGASE/src/br/com/ufc/es/poo/model/BolsistaVoluntario.java
@@ -6,15 +6,13 @@ import java.util.ArrayList;
 
 public class BolsistaVoluntario extends AlunoBolsista {
 
-    public BolsistaVoluntario(){
-        
+    public BolsistaVoluntario() {
     }
-    public BolsistaVoluntario(String cpf, String senha, String email, String nome, 
-            int matricula, double mediaAluno,CursosOfertados cursoMatriculado){
-        super(cpf, senha, email, nome, 
-            matricula, mediaAluno,cursoMatriculado);
+
+    public BolsistaVoluntario(String cpf, String senha, String email, String nome, int matricula, CursosOfertados cursoMatriculado) {
+        super(cpf, senha, email, nome, matricula, cursoMatriculado);
     }
-   
+
     
     public void selecionaProjeto(Projeto projeto ) {
         ArrayList<AlunoBolsista> alunos = projeto.getAlunos();
